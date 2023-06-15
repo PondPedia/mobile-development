9097048e396fd20cfc95bec957aead5b89258bcd
dimashdanud <dhdanudahlan@gmail.com>
2023-06-15 17:06:58 +0700

    Add Dagger-Hilt DI

---
+++ app/src/main/java/com/aetherized/compose/pondpedia/di/PondLogModule.kt
@@ -0,0 +1,60 @@
+package com.aetherized.compose.pondpedia.di
+
+import android.app.Application
+import androidx.room.Room
+import com.aetherized.compose.pondpedia.data.local.dao.PondLogDao
+import com.aetherized.compose.pondpedia.data.local.database.PondDatabase
+import com.aetherized.compose.pondpedia.data.remote.api.PondApi
+import com.aetherized.compose.pondpedia.data.repository.PondLogRepositoryImpl
+import com.aetherized.compose.pondpedia.data.util.GsonParser
+import com.aetherized.compose.pondpedia.domain.repository.PondLogRepository
+import com.aetherized.compose.pondpedia.domain.use_case.GetPondLogById
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PondLogModule {
+
+    @Provides
+    @Singleton
+    fun provideGetPondLogUseCase(repository: PondLogRepository): GetPondLogById {
+        return GetPondLogById(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun providePondLogRepository(
+        api: PondApi,
+        dao: PondLogDao,
+    ): PondLogRepository {
+        return PondLogRepositoryImpl(api, dao)
+    }
+
+    @Provides
+    @Singleton
+    fun providePondDatabase(app: Application): PondDatabase {
+        return Room.databaseBuilder(
+            app,
+            PondDatabase::class.java,
+            "pond_database_v1.0"
+        ).addTypeConverter(GsonParser(Gson()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePondApi(): PondApi {
+        return Retrofit.Builder()
+            .baseUrl(PondApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PondApi::class.java)
+    }
+}
