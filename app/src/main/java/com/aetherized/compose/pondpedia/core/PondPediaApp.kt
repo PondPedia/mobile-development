a62bb315ff1e46f9a4eb5f4deb9a0b10c40f6a9a
dimashdanud <dhdanudahlan@gmail.com>
2023-06-15 17:06:08 +0700

    Added @HiltAndroidApp annotation

--- app/src/main/java/com/aetherized/compose/pondpedia/core/PondPediaApp.kt
+++ app/src/main/java/com/aetherized/compose/pondpedia/core/PondPediaApp.kt
@@ -4,8 +4,9 @@ import android.app.Application
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class PondPediaApp: Application() {
     val dataStore: DataStore<Preferences> by preferencesDataStore(name = "pondpedia_datastore")
     lateinit var pondPediaDataStore: PondPediaDataStore
